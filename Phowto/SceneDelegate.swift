f539074b592607be722db88e497fb40fe83a4fd9
blackver69 <Rickychen930@gmail.com>
2022-04-07 19:31:00 +0700

    Revert "penambahan fitur carousel dengan cara pembuatan table secara manual"
    This reverts commit 33cd0fa15ec2af9fd198e114aa33bdd19ff977d0.

--- Phowto/SceneDelegate.swift
+++ Phowto/SceneDelegate.swift
@@ -16,17 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-//        guard let _ = (scene as? UIWindowScene) else { return }
-        //perubahan
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-                        
-                        window = UIWindow(windowScene: windowScene)
-
-                let root = UIStoryboard(name: "HomePage", bundle: nil)
-                let initialController = root.instantiateViewController(withIdentifier: "HomePageSB")
-        
-                window?.rootViewController = initialController
-                        window?.makeKeyAndVisible()
+        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
